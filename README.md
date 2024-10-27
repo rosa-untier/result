b266cb395ee53a5964227fc0c0c532140f88c2b5
vinpogo <vgrossmayer@gmail.com>
2024-10-27 17:05:57 +0100

    doc: add module documentation

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# random
+# @untier/result
+
+A minimalistic implementation of a result type.
+
+The advantage of using a result type compared to throwing exceptions, is
+transparency in the consuming code. Compared to `throw` this approach does not
+hide the fact that unexpected results might occur.
--- mod.ts
+++ mod.ts
@@ -1,2 +1,14 @@
+/**
+ * @module
+ * @example
+ * ```ts
+ * const divide = (a,b) => {
+ *   if (b <= 0) return new NotOk('cannot divide by 0')
+ *   return new Ok(a / b)
+ * }
+ *
+ * const value = unwrap(divide(10, 0), Infinity) // returns Infinity
+ * ```
+ */
 export * from "./src/utils.ts";
 export * from "./src/types.ts";
