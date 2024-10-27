efcca518bbc8d58ea9c1fa14f9769b40eee9a22e
vinpogo <vgrossmayer@gmail.com>
2024-10-27 16:57:49 +0100

    doc: add docs to types and classes

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,4 @@
+/** A union type of either `Ok` or `NotOk` */
 export type Result<O, N> = Ok<O> | NotOk<N>;
 
 class PrivateValue<T> {
@@ -11,6 +12,8 @@ class PrivateValue<T> {
   }
 }
 
+/** a simple data container for an `Ok` result */
 export class Ok<O> extends PrivateValue<O> {}
 
+/** a simple data container for a `NotOk` result */
 export class NotOk<N> extends PrivateValue<N> {}
